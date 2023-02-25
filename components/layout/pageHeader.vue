ca7fc512efc9cee7d890156aa0911aa0123b62b0
Kyle <kylezimmer2@gmail.com>
2023-02-25 08:12:42 -0600

    customer page work

---
+++ components/layout/pageHeader.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+interface action {
+  link: string;
+  label: string;
+  icon: string;
+  action: () => void;
+}
+
+const props = defineProps({
+  title: {
+    type: String,
+    default: ''
+  },
+  actions: {
+    type: Array<action>,
+    default: []
+  }
+});
+</script>
+<template>
+  <div class="flex justify-between">
+    <h1>{{ props.title }}</h1>
+    <div class="flex">
+      <div v-for="action of actions" @click="action.action">
+        <font-awesome-icon :icon="['fas', `${action.icon}`]" />{{ action.label }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- pages/customers/index.vue
+++ pages/customers/index.vue
@@ -1,4 +1,10 @@
 <script setup lang="ts">
+type customer = {
+  firstName: string;
+  lastName: string;
+  phone: string;
+};
+
 const { $client } = useNuxtApp();
 
 const customers = await $client.customer.list.query();
@@ -11,6 +17,17 @@ const form = reactive({
   phone: ''
 });
 
+let selectedCustomer = $ref(null as unknown as customer);
+let customerModalOpen = $ref(false);
+
+const openCustomerModal = () => {
+  customerModalOpen = !customerModalOpen;
+};
+
+const selectCustomer = (customer: customer) => {
+  selectedCustomer = customer;
+};
+
 const addCustomer = async () => {
   try {
     const customer = await $client.customer.add.mutate(form);
@@ -22,26 +39,50 @@ const addCustomer = async () => {
 </script>
 
 <template>
-  <div class="flex space-x-4 h-full px-4 py-3">
-    <ul role="list" class="divide-y divide-gray-200 bg-white py-4 px-6 rounded-xl">
-      <li v-for="customer in customers" :key="customer.id" class="flex py-4">
-        <img v-if="customer.image" class="h-14 w-14 rounded-full" :src="customer.image" alt="" />
-        <div v-else class="h-14 w-14 rounded-full bg-gray-200 flex justify-center items-center">
-          <font-awesome-icon :icon="['fas', 'user']" class="text-lg text-gray-600" />
-        </div>
-        <div class="ml-3 flex flex-col justify-between py-1">
-          <p class="text-sm font-bold text-gray-800">{{ customer.firstName }} {{ customer.lastName }}</p>
-          <p v-if="customer.phone" class="text-sm text-gray-500">{{ formatPhone(customer.phone) }}</p>
-        </div>
-      </li>
-    </ul>
-    <div class="bg-white p-4 rounded-xl flex-1 shadow-lg">
-      <form class="flex flex-col" @submit.prevent="addCustomer">
-        <input v-model="form.firstName" type="text" placeholder="First name" />
-        <input v-model="form.lastName" type="text" placeholder="Last name" />
-        <input v-model="form.phone" type="text" placeholder="Phone" />
-        <button type="submit">Add customer</button>
-      </form>
+  <div>
+    <LayoutPageHeader
+      title="Customers"
+      :actions="[
+        {
+          label: 'Create',
+          link: '/customers=',
+          icon: 'plus',
+          action: openCustomerModal
+        }
+      ]"
+    />
+    <div class="flex space-x-4 h-full px-4 py-3">
+      <ul role="list" class="divide-y divide-gray-200 bg-white py-4 px-6 rounded-xl">
+        <li
+          v-for="customer in customers"
+          :key="customer.id"
+          class="flex py-4 cursor-pointer"
+          @click="selectCustomer(customer as customer)"
+        >
+          <img v-if="customer.image" class="h-14 w-14 rounded-full" :src="customer.image" alt="" />
+          <div v-else class="h-14 w-14 rounded-full bg-gray-200 flex justify-center items-center">
+            <font-awesome-icon :icon="['fas', 'user']" class="text-2xl text-gray-600" />
+          </div>
+          <div class="ml-3 flex flex-col justify-between py-1">
+            <p class="font-bold text-gray-800">{{ customer.firstName }} {{ customer.lastName }}</p>
+            <p v-if="customer.phone" class="text-sm text-gray-500">{{ formatPhone(customer.phone) }}</p>
+          </div>
+        </li>
+      </ul>
+      <div class="bg-white p-4 rounded-xl flex-1 shadow-lg">
+        <transition name="slide-up">
+          <form v-if="customerModalOpen" class="flex flex-col" @submit.prevent="addCustomer">
+            <input v-model="form.firstName" type="text" placeholder="First name" />
+            <input v-model="form.lastName" type="text" placeholder="Last name" />
+            <input v-model="form.phone" type="text" placeholder="Phone" />
+            <button type="submit">Add customer</button>
+          </form>
+          <div v-else-if="selectedCustomer">
+            <h2>{{ selectedCustomer.firstName }} {{ selectedCustomer.lastName }}</h2>
+          </div>
+          <div v-else>Dashboard!</div>
+        </transition>
+      </div>
     </div>
   </div>
 </template>
